f9ebcf7f93f60f32e0a3209abaf342c422bc224c
tealreal <165613688+tealreal@users.noreply.github.com>
2024-07-28 10:48:56 -1000

    1.0.4 - fix logging crash
    this took too much time to figure out.

--- src/main/java/teal/hdhead/HeadClient.java
+++ src/main/java/teal/hdhead/HeadClient.java
@@ -17,8 +17,8 @@ import java.util.List;
 
 public final class HeadClient implements ClientModInitializer {
 
-    private static final ConfigObject config = ConfigObject.getConfigObject();
     public static final Logger logger = LoggerFactory.getLogger(HeadClient.class);
+    private static final ConfigObject config = ConfigObject.getConfigObject();
 
     private static boolean runMod = true;
 
